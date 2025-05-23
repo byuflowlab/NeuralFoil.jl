15fa0b2135e7bf1246d685951071e69265a6f855
Documenter.jl <documenter@juliadocs.github.io>
2025-05-23 18:31:42 +0000

    build based on fc09253

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#NeuralFoil","page":"Home","title":"NeuralFoil","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for NeuralFoil.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
